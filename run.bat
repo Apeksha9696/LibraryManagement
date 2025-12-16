03b546928cf25e78f55fdb76a9b567a4fb367b6f
Apeksha9696 <151911391+Apeksha9696@users.noreply.github.com>
2025-12-16 13:45:45 +0530

    add-member.html

--- run.bat
+++ run.bat
@@ -1,9 +1,3 @@
 @echo off
-echo Starting Library Management System...
-echo.
-mvn spring-boot:run
-echo.
-echo Application is running at: http://localhost:9000
-echo Press any key to open in browser...
-pause >nul
-start http://localhost:9000
+java -jar LibraryManagement.jar
+pause
